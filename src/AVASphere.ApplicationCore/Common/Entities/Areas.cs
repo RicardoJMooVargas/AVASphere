10dae6bd46244468a61bfc76f08b45ea1c0066ce
Angel160402 <al066132@uacam.mx>
2025-10-24 11:41:19 -0600

    Creación de clases en la carpeta Entities dentro de Common

---
+++ src/AVASphere.ApplicationCore/Common/Entities/Areas.cs
@@ -0,0 +1,8 @@
+namespace AVASphere.ApplicationCore.Common.Entities;
+
+public class Areas
+{
+    public int IdAreas { get; set; }
+    public string? Name { get; set; }
+    public string? NormalizedName { get; set; }
+}
--- src/AVASphere.ApplicationCore/Projects/Entities/Visits.cs
+++ src/AVASphere.ApplicationCore/Projects/Entities/Visits.cs
@@ -2,5 +2,7 @@ namespace AVASphere.ApplicationCore.Projects.Entities;
 
 public class Visits
 {
-    
+    public int IdVisits { get; set; }
+    public string? Image { get; set; }
+    public string? Description { get; set; }
 }

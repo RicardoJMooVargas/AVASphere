2093d05cd8cb9d8f20e167104ab3316946896984
ricardoMogas <al066208@uacam.mx>
2025-10-24 12:39:22 -0600

    aplicación de migración

--- src/AVASphere.ApplicationCore/Common/Entities/Areas.cs
+++ src/AVASphere.ApplicationCore/Common/Entities/Areas.cs
@@ -1,8 +1,11 @@
 namespace AVASphere.ApplicationCore.Common.Entities;
 
+
 public class Areas
 {
     public int IdAreas { get; set; }
     public string? Name { get; set; }
     public string? NormalizedName { get; set; }
+
+    public ICollection<Rols> Rols { get; set; } = new List<Rols>();
 }

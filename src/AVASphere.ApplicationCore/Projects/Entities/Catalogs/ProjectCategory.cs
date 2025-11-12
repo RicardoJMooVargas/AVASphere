f491780f90fd9ee9668843a8947ddd574292a6d2
ricardoMogas <al066208@uacam.mx>
2025-11-12 08:53:01 -0600

    revisión de modelos.

--- src/AVASphere.ApplicationCore/Projects/Entities/Catalogs/ProjectCategory.cs
+++ src/AVASphere.ApplicationCore/Projects/Entities/Catalogs/ProjectCategory.cs
@@ -8,13 +8,12 @@ public class ProjectCategory
     public int IdProjectCategory { get; set; }
     public string? Name { get; set; }
     public string? NormalizedName { get; set; }
-    
-    // FK 
-   // public int IdConfigSys { get; set; }
-   // public ConfigSys ConfigSys { get; set; } = null!;
-    // Relaciones 
-   // public ICollection<IndividualProjectQuote> IndividualProjectQuotes { get; set; } = new List<IndividualProjectQuote>();
-   // public ICollection<ListOfCategories> ListOfCategories { get; set; } = new List<ListOfCategories>();
-   // public ICollection<ListOfProductsByCategory> ListOfProductsByCategory { get; set; } = new List<ListOfProductsByCategory>();
-   // public ICollection<TechnicalDesign> TechnicalDesigns { get; set; } = new List<TechnicalDesign>();
+    // FK
+    public int IdConfigSys { get; set; }
+    public ConfigSys ConfigSys { get; set; } = null!;
+   // Relaciones 
+    public ICollection<IndividualProjectQuote> IndividualProjectQuotes { get; set; } = new List<IndividualProjectQuote>();
+    public ICollection<ListOfCategories> ListOfCategories { get; set; } = new List<ListOfCategories>();
+    public ICollection<ListOfProductsByCategory> ListOfProductsByCategory { get; set; } = new List<ListOfProductsByCategory>();
+    public ICollection<TechnicalDesign> TechnicalDesigns { get; set; } = new List<TechnicalDesign>();
 }
--- src/AVASphere.Infrastructure/Projects/Configuration/ProjectCategoryEntitieConfig.cs
+++ src/AVASphere.Infrastructure/Projects/Configuration/ProjectCategoryEntitieConfig.cs
@@ -4,27 +4,42 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AVASphere.Infrastructure.Projects.Configuration;
 
-    public class ProjectCategoryEntitieConfig : IEntityTypeConfiguration<ProjectCategory>
+public class ProjectCategoryEntitieConfig : IEntityTypeConfiguration<ProjectCategory>
+{
+    public void Configure(EntityTypeBuilder<ProjectCategory> entity)
     {
-        public void Configure(EntityTypeBuilder<ProjectCategory> entity)
-        {
-            // Ejemplo de configuración:
-            entity.ToTable("ProjectCategory");
-            entity.HasKey(e => e.IdProjectCategory); 
-            
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .IsRequired();
-            
-            entity.Property(e => e.NormalizedName)
-                .HasMaxLength(100)
-                .IsRequired();
-            
-            // RELACIONES
-           /* entity.HasMany(pc => pc.IndividualProjectQuotes) // ICollection<IndividualProjectQuote>
-                .WithOne(ipq => ipq.ProjectCategory) // IndividualProjectQuote.ProjectCategory
-                .HasForeignKey(ipq => ipq.IdProjectCategory) // FK en IndividualProjectQuote
-                .OnDelete(DeleteBehavior.Cascade);
-                */
-        }
+        // Ejemplo de configuración:
+        entity.ToTable("ProjectCategory");
+        entity.HasKey(e => e.IdProjectCategory); 
+        
+        entity.Property(e => e.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        entity.Property(e => e.NormalizedName)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        // FK to ConfigSys configured in ConfigSysEntitieConfig
+        
+       entity.HasMany(pc => pc.IndividualProjectQuotes) // ICollection<IndividualProjectQuote>
+            .WithOne(ipq => ipq.ProjectCategory) // IndividualProjectQuote.ProjectCategory
+            .HasForeignKey(ipq => ipq.IdProjectCategory) // FK en IndividualProjectQuote
+            .OnDelete(DeleteBehavior.Cascade);
+       
+       entity.HasMany(pc => pc.ListOfCategories)
+            .WithOne(loc => loc.ProjectCategory)
+            .HasForeignKey(loc => loc.IdProjectCategory)
+            .OnDelete(DeleteBehavior.Cascade);
+       
+       entity.HasMany(pc => pc.ListOfProductsByCategory)
+            .WithOne(lopbc => lopbc.ProjectCategory)
+            .HasForeignKey(lopbc => lopbc.IdProjectCategory)
+            .OnDelete(DeleteBehavior.Cascade);
+       
+       entity.HasMany(pc => pc.TechnicalDesigns)
+            .WithOne(td => td.ProjectCategory)
+            .HasForeignKey(td => td.IdProjectCategory)
+            .OnDelete(DeleteBehavior.Cascade);
     }
+}

b0a78477884a9ef65a0db84e7e3480bca9c5b2a8
Angel Hidalgo <al066132@uacam.mx>
2025-11-25 15:58:11 -0600

    Se creó una nueva rama feature/implement-catalog-property

---
+++ src/AVASphere.ApplicationCore/Common/DTOs/PropertyDTOs.cs
@@ -0,0 +1,14 @@
+﻿namespace AVASphere.ApplicationCore.Common.DTOs;
+
+public class PropertyRequestDto
+{
+    public string Name { get; set; } = null!;
+    public string? NormalizedName { get; set; }
+}
+
+public class PropertyResponseDto
+{
+    public int IdProperty { get; set; }
+    public string Name { get; set; } = null!;
+    public string? NormalizedName { get; set; }
+}
---
+++ src/AVASphere.ApplicationCore/Common/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,22 @@
+﻿using AVASphere.ApplicationCore.Common.Entities.Catalogs;
+
+namespace AVASphere.ApplicationCore.Common.Interfaces;
+
+public interface IPropertyRepository
+{
+    // Create
+    Task<Property> CreateAsync(Property property);
+    
+    // Read
+    Task<Property?> GetByIdAsync(int id);
+    Task<Property?> GetByNameAsync(string name);
+    Task<IEnumerable<Property>> GetAllAsync();
+    Task<bool> ExistsAsync(int id);
+    Task<bool> ExistsByNameAsync(string name);
+    
+    // Update
+    Task<Property> UpdateAsync(Property property);
+    
+    // Delete
+    Task<bool> DeleteAsync(int id);
+}
